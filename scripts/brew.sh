978caf0e5ad1fd3c099192a78746ddca9a6811a9
Alexander Untch <untcha@gmail.com>
2017-02-23 22:19:11 +0100

    fixed newline and added tailing ;

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 if [[ -e `which brew` ]]; then
-	echo "Hombrew is already installed."
+	echo "Hombrew is already installed.";
 else
-	echo "Installing Hombrew..."
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	echo "Done..."
+	echo "Installing Hombrew...";
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+	echo "Done...";
 fi
 
-echo "Updating Homebrew..."
-brew update
+echo "Updating Homebrew...";
+brew update;
 
-read -p "Would you like to install brews? [y/n]" -n 1 -r
+read -p "Would you like to install brews? [y/n]" -n 1;
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	echo -n "Installing..."
-	source ../brew/brewfile.sh
-	install_brews
+	echo " ";
+	echo "Installing...";
+	source ../brew/brewfile.sh;
+	install_brews;
 fi
