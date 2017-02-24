9c31357606fcaba48f6ed4f53551f0385ac19f05
Alexander Untch <untcha@gmail.com>
2017-02-24 15:05:04 +0100

    added zsh-syntax-highlightning

--- scripts/oh-my-zsh-iterm2.sh
+++ scripts/oh-my-zsh-iterm2.sh
@@ -9,8 +9,9 @@ if [[ -e "/usr/local/bin/zsh" ]];
 then
 	echo "zsh is already installed via homebrew!";
 else
-	echo "zsh is NOT installed! Installing via homebrew...";
+	echo "zsh is NOT installed! Installing zsh via homebrew...";
 	brew install zsh;
+	brew install zsh-syntax-highlighting;
 	echo "zsh is now installed!";
 fi
 
@@ -23,4 +24,6 @@ else
 	echo "...";
 	sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells" && chsh -s /usr/local/bin/zsh;
 	echo "zsh is now set as default shell!";
+	echo " ";
+	echo "Make sure to exit your current Terminal session and open Terminal again. Select 0 if asked by zsh! Run this script again!"
 fi
