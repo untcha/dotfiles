42d7da5f92acb26f3b93d9c3cef0ac5720e3dc0c
Alexander Untch <untcha@gmail.com>
2017-02-24 15:20:02 +0100

    modified oh-my-zsh-iterm2.sh

--- scripts/oh-my-zsh-iterm2.sh
+++ scripts/oh-my-zsh-iterm2.sh
@@ -33,7 +33,7 @@ fi
 ###############################################################################
 
 # Check if Oh-My-Zsh is installed; install if not
-if [[ -d "~/.oh-my-zsh" ]];
+if [[ -e "~/.oh-my-zsh/oh-my-zsh.sh" ]];
 then
 	echo "Oh-My-Zsh is already installed!";
 else
