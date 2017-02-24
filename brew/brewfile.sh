5639a65c1c28f730322ec5805a572934e6d63eea
Alexander Untch <untcha@gmail.com>
2017-02-24 13:46:48 +0100

    removed jenv and zsh

--- brew/brewfile.sh
+++ brew/brewfile.sh
@@ -7,13 +7,10 @@ install_brews() {
 	# Install Formulae
 	brew install fping
 	brew install git
-	brew install jenv
 	brew install mas
 	brew install prettyping
 	brew install tree
 	brew install tvnamer
-	brew install zsh
-	brew install zsh-syntax-highlighting
 	
 	# Clean up
 	brew cleanup
