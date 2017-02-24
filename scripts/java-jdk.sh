3bf05c22b4bfaba8532b5fd4b08c74338368f258
Alexander Untch <untcha@gmail.com>
2017-02-24 20:00:23 +0100

    added java7 and java8 installation

---
+++ scripts/java-jdk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DOTFILE_STRING="[.dotfiles/scripts/java-jdk.sh] ";
+WRENCH_EMOJI=$'\xF0\x9F\x94\xA7';
+SHELL_EMOJI=$'\xF0\x9F\x90\x9A';
+FIRE_EMOJI=$'\xF0\x9F\x94\xA5';
+OK_EMOJI=$'\xE2\x9C\x85';
+NOK_EMOJI=$'\xE2\x9B\x94';
+
+# after java
+# ant maven jenv bazel
+
+###############################################################################
+# Install Java 7 and Java 8                                                   #
+###############################################################################
+
+# Check if Java 7 is installed; install if not
+if ls /Library/Java/JavaVirtualMachines/jdk1.7.* 1> /dev/null 2>&1;
+then
+	echo $DOTFILE_STRING $OK_EMOJI ": Java 7 is already installed!";
+else
+	echo $DOTFILE_STRING $NOK_EMOJI ": Java 7 is NOT installed!";
+	echo $DOTFILE_STRING $WRENCH_EMOJI ": Installing Java 7...";
+	brew update;
+	brew cask install java7;
+	echo $DOTFILE_STRING $OK_EMOJI ": Java 7 is now installed!";
+fi
+
+# Check if Java 8 is installed; install if not
+if ls /Library/Java/JavaVirtualMachines/jdk1.8.* 1> /dev/null 2>&1;
+then
+	echo $DOTFILE_STRING $OK_EMOJI ": Java 8 is already installed!";
+else
+	echo $DOTFILE_STRING $NOK_EMOJI ": Java 8 is NOT installed!";
+	echo $DOTFILE_STRING $WRENCH_EMOJI ": Installing Java 7...";
+	brew update;
+	brew cask install java;
+	echo $DOTFILE_STRING $OK_EMOJI ": Java 8 is now installed!";
+fi
