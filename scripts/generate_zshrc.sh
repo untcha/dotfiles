af1bb8c71256da28166a2be59f0c566b5f202df1
Alexander Untch <untcha@gmail.com>
2017-02-25 16:48:12 +0100

    Modified generate_zshrc.sh

--- scripts/generate_zshrc.sh
+++ scripts/generate_zshrc.sh
@@ -2,4 +2,12 @@
 
 source ./global_functions.sh;
 
-echo $global_dotfiles_dir;
+zshrc_custom=$global_dotfiles_dir/.zshrc;		# custom .zshrc source file
+zshrc_destination=$HOME/.zshrc					# destination for the custom .zshrc file
+
+if [ -f $zshrc_destination ]; then
+	rm $zshrc_destination;
+	cat $zshrc_custom > $zshrc_destination;
+else
+	cat $zshrc_custom > $zshrc_destination;
+fi
