531520b83ee7be8bcdfcea45a6eb1315710c25ea
Alexander Untch <untcha@gmail.com>
2017-02-25 18:10:03 +0100

    Modified generate_zshrc.sh

--- scripts/generate_zshrc.sh
+++ scripts/generate_zshrc.sh
@@ -14,13 +14,20 @@ fi
 
 
 # Modify .zshrc DEFAULT_USER
-search_string_user="DEFAULT_USER=\"default_user\"";
-replace_string_user="DEFAULT_USER=\"$(whoami)\"";
+#search_string_user="DEFAULT_USER=\"default_user\"";
+#replace_string_user="DEFAULT_USER=\"$(whoami)\"";
 
-sed -i '' 's/'"$search_string_user"'/'"$replace_string_user"'/g' "$zshrc_destination";
+#sed -i '' 's/'"$search_string_user"'/'"$replace_string_user"'/g' "$zshrc_destination";
 
 # Modify .zshrc installation path
-search_string_path="export ZSH\=\/Users\/default_user\/.oh-my-zsh";
-replace_string_path="export ZSH\=\/Users\/$(whoami)\/.oh-my-zsh";
+#search_string_path="export ZSH\=\/Users\/default_user\/.oh-my-zsh";
+#replace_string_path="export ZSH\=\/Users\/$(whoami)\/.oh-my-zsh";
 
-sed -i '' 's/'"$search_string_path"'/'"$replace_string_path"'/g' "$zshrc_destination";
+#sed -i '' 's/'"$search_string_path"'/'"$replace_string_path"'/g' "$zshrc_destination";
+
+
+
+search_string="CURRENT_USER";
+replace_string="$(whoami)";
+
+sed -i '' 's/'"$search_string"'/'"$replace_string"'/g' "$zshrc_destination";
