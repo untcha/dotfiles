3ea9310c688879315de5bfad9efd41879807ce9d
Alexander Untch <untcha@gmail.com>
2017-02-25 20:45:00 +0100

    Modified generate_files.sh

---
+++ scripts/generate_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+me=$BASH_SOURCE;
+source ./global_functions.sh
+
+read -p "Would you like to generate the ssh config file? [y/n]" -n 1;
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+	echo "[$me]"$EMOJI_WRENCH ": Generating ssh-config...";
+	source ./generate_ssh-config.sh
+	generate_ssh_config;
+fi
+
+read -p "Would you like to generate .zshrc file? [y/n]" -n 1;
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+	echo "[$me]"$EMOJI_WRENCH ": Generating .zshrc...";
+	source ./generate_zshrc.sh
+	generate_zshrc;
+	echo "[$me]"$EMOJI_WRENCH ": Modifying .zshrc...";
+	modify_zshrc;
+fi
