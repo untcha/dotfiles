3292f626ab763b3260d3b1c41b5db5bec7aac500
Alexander Untch <untcha@gmail.com>
2017-02-25 20:37:01 +0100

    Modified generate_ssh-config.sh

---
+++ scripts/generate_ssh-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ./global_functions.sh;
+
+ssh_config_custom=$global_dotfiles_dir/ssh/config;		# custom ssh config source file
+ssh_config_destination=$HOME/.ssh/config				# destination for the custom ssh config file
+
+# Generate ssh config file from the template .dotfiles/ssh/config
+generate_ssh_config() {
+	if [ -f $ssh_config_destination ]; then
+		rm $ssh_config_destination;
+		cat $ssh_config_custom > $ssh_config_destination;
+	else
+		cat $ssh_config_custom > $ssh_config_destination;
+	fi
+}
