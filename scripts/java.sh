31ff477cc2627198fe84b379cee5a64613f98193
Alexander Untch <untcha@gmail.com>
2017-02-28 21:18:50 +0100

    Refactored java.sh

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
-DOTFILE_STRING="[.dotfiles/scripts/java-jdk.sh] ";
-WRENCH_EMOJI=$'\xF0\x9F\x94\xA7';
-SHELL_EMOJI=$'\xF0\x9F\x90\x9A';
-FIRE_EMOJI=$'\xF0\x9F\x94\xA5';
-OK_EMOJI=$'\xE2\x9C\x85';
-NOK_EMOJI=$'\xE2\x9B\x94';
-
 ###############################################################################
-# Install Java 7 and Java 8                                                   #
+# This script installs Java JDK 7 and Java JDK 8 (via cask),
+# jenv (via homebrew) and configures jenv
 ###############################################################################
 
-# Check if Java 7 is installed; install if not
+me=$BASH_SOURCE;
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+	source "$DIR/global_functions.sh";
+
+###############################################################################
+# Install Java JDK 7 and Java JDK 8
+###############################################################################
+
+# Check if Java JDK 7 is installed; install if not
 if ls /Library/Java/JavaVirtualMachines/jdk1.7.* 1> /dev/null 2>&1;
 then
-	echo $DOTFILE_STRING $OK_EMOJI ": Java 7 is already installed!";
+	echo "[$me]"$EMOJI_OK ": Java JDK 7 is already installed!";
 else
-	echo $DOTFILE_STRING $NOK_EMOJI ": Java 7 is NOT installed!";
-	echo $DOTFILE_STRING $WRENCH_EMOJI ": Installing Java 7...";
+	echo "[$me]"$EMOJI_NOK ": Java JDK 7 is NOT installed!";
+	echo "[$me]"$EMOJI_WRENCH ": Installing Java JDK 7...";
 	brew update;
 	brew cask install java7;
-	echo $DOTFILE_STRING $OK_EMOJI ": Java 7 is now installed!";
+	echo "[$me]"$EMOJI_OK ": Java JDK 7 is now installed!";
 fi
 
-# Check if Java 8 is installed; install if not
+# Check if Java JDK 8 is installed; install if not
 if ls /Library/Java/JavaVirtualMachines/jdk1.8.* 1> /dev/null 2>&1;
 then
-	echo $DOTFILE_STRING $OK_EMOJI ": Java 8 is already installed!";
+	echo "[$me]"$EMOJI_OK ": Java JDK 8 is already installed!";
 else
-	echo $DOTFILE_STRING $NOK_EMOJI ": Java 8 is NOT installed!";
-	echo $DOTFILE_STRING $WRENCH_EMOJI ": Installing Java 8...";
+	echo "[$me]"$EMOJI_NOK ": Java JDK 8 is NOT installed!";
+	echo "[$me]"$EMOJI_WRENCH ": Installing Java JDK 8...";
 	brew update;
 	brew cask install java;
-	echo $DOTFILE_STRING $OK_EMOJI ": Java 8 is now installed!";
+	echo "[$me]"$EMOJI_OK ": Java JDK 8 is now installed!";
 fi
 
 ###############################################################################
-# Install and configure jenv                                                  #
+# Install and configure jenv
 ###############################################################################
 
-echo $DOTFILE_STRING $WRENCH_EMOJI ": Installing jenv...";
+echo "[$me]"$EMOJI_WRENCH ": Installing jenv...";
 brew update;
 brew install jenv;
 
@@ -56,7 +60,7 @@ jenv add $JDK8_PATH;
 jenv rehash
 
 ###############################################################################
-# Install brews who need Java                                                 #
+# Install brews who need Java
 ###############################################################################
 brew update;
 brew install ant;
