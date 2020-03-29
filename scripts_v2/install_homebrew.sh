dac228909c221d65f4df4d3767643934077b542b
Alexander Untch <alexander@untch.com>
2020-03-29 17:08:22 +0200

    added more colors

--- scripts_v2/install_homebrew.sh
+++ scripts_v2/install_homebrew.sh
@@ -4,15 +4,20 @@
 # This script installs homebrew
 ###############################################################################
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+
 me=$BASH_SOURCE;
 
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 if [[ -e `which brew` ]]; then
-	echo "Homebrew is already installed!";
+	echo -e "${YELLOW}Homebrew is already installed!${NC}";
 else
-	echo "Homebrew is NOT installed!";
+	echo "Homebrew is not installed!";
 	echo "Installing Homebrew...";
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 	echo "Homebrew is now installed!";
