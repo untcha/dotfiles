ab3d574e6652bcfc377c27fcd2555d934e7ae458
Alexander Untch <alexander@untch.com>
2020-03-29 16:41:28 +0200

    improved package installation

--- scripts_v2/brewfile.sh
+++ scripts_v2/brewfile.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+packages=('clamav'
+	  'git'
+	  'jenv'
+	  'mas'
+	  'mpssh'
+	  'screenresolution'
+	  'topgrade'
+	  'wget'
+	  'zsh')
+
+
 install_brew_packages() {
 	# Update brew
 	brew update
 
 	# Install packages
-	echo "Installing git..."
+	for i in ${!packages[@]}; do
+		is_installed=true;
+		( brew list --versions ${packages[$i]} > /dev/null) || is_installed=false;		
+
+		if [[ $is_installed == true ]];
+		then
+			echo -e "--> ${GREEN}${packages[$i]}${NC}" "is already installed!";
+		else
+			echo -e "--> ${RED}${packages[$i]}${NC}" "is not installed!";
+			echo -e "    installing ${RED}${packages[$i]}${NC}";
+			echo -e "    successfully installed ${GREEN}${packages[$i]}${NC}";
+		fi
+	done
 }
