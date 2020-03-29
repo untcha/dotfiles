b2b5ffcaf9d5834402a222ed6c1a233eb0249402
Alexander Untch <alexander@untch.com>
2020-03-29 19:34:07 +0200

    improved displayplacer command

--- scripts_v2/config_macos.sh
+++ scripts_v2/config_macos.sh
@@ -15,17 +15,13 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 #scutil --get LocalHostName
-
 resolution="1680x1050"
-echo "$resolution"
 
 # Get the persistent screen id
 persistent_screen_id=$(displayplacer list | grep 'Persistent screen id:' | awk '{print $4}')
-echo "$persistent_screen_id"
 
 # Get color depth value
 color_depth=$(displayplacer list | grep 'Color Depth:' | awk '{print $3}')
-echo "$color_depth"
 
 # Set screen resolution with displayplacer
 displayplacer "id:$persistent_screen_id res:$resolution color_depth:$color_depth scaling:on origin:(0,0) degree:0"
