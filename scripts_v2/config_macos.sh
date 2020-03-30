b9de7d0e5c89e73cbeaca520ae62493aa842af3f
Alexander Untch <alexander@untch.com>
2020-03-30 15:52:08 +0200

    improved Notification settings

--- scripts_v2/config_macos.sh
+++ scripts_v2/config_macos.sh
@@ -86,15 +86,21 @@ defaults write NSGlobalDomain AppleMetricUnits -int 1
 # System Preferences/Notifications                                            #
 ###############################################################################
 
+# Do Not Disturb settings
+# Set "When the screen is locked" to true (1)
 defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui dndEnabledDisplayLock -int 1
 
+# Set "When display is sleeping" to true (1)
 defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui dndEnabledDisplaySleep -int 1
 
+# Set start value for scheduled DND (calc example: 15:00 --> 15 * 60 + 0 = 900; 18:30 --> 18 * 60 + 30 = 1110) 
 defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui dndStart -int 1380
 
+# Set end value for scheduled DND (calc example: 15:00 --> 15 * 60 + 0 = 900; 18:30 --> 18 * 60 + 30 = 1110)
 defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui dndEnd -int 480
 
-defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -int 0
+# ?
+defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -int 1
 killall NotificationCenter
 
 ###############################################################################
