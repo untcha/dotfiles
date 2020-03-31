3fb976db942ba2c24f58efdf6017e78794bdcfe8
Alexander Untch <alexander@untch.com>
2020-03-31 07:45:27 +0200

    modified Trackpad settings

--- scripts_v2/config_macos.sh
+++ scripts_v2/config_macos.sh
@@ -151,8 +151,12 @@ defaults write com.apple.HIToolbox AppleSelectedInputSources -array '
 ###############################################################################
 
 # Set "Tap to click" to true (1)
-defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
-
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write Apple Global Domain com.apple.mouse.tapBehavior 1
+#
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
 
 ###############################################################################
 # System Preferences/Displays                                                 #
