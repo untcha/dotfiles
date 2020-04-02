b61146c08e79442e9d79cc61b07a92626949332d
Alexander Untch <alexander@untch.com>
2020-04-02 20:26:06 +0200

    Modified utils.sh

--- src/os/install/macos/utils.sh
+++ src/os/install/macos/utils.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+brew_tap() {
+	brew tap "$1" &> /dev/null
+}
+
+brew_update() {
+	
+	execute \
+		"brew update" \
+		"Homebrew (update)"
+	
+}
+
+brew_upgrade() {
+	
+	execute \
+		"brew upgrade" \
+		"Homebrew (upgrade)"
+	
+}
