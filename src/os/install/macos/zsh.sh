fed432326a1a872366b500f24ddcf3ed50496b6f
Alexander Untch <alexander@untch.com>
2020-04-03 18:27:29 +0200

    Adapted change_default_shell function for zsh

--- src/os/install/macos/zsh.sh
+++ src/os/install/macos/zsh.sh
@@ -6,18 +6,60 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-
+change_default_shell() {
+	
+	local brewPrefix=""
+	local newShellPath=""
+	
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	# Try to get the path of the `Zsh`
+	# version installed through `Homebrew`.
+	
+	brewPrefix="$(brew_prefix)" \
+        || return 1
+	
+	newShellPath="$brewPrefix/bin/zsh"
+	
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	# Add the path of the `Bash` version installed through `Homebrew`
+	# to the list of login shells from the `/etc/shells` file.
+	#
+	# This needs to be done because applications use this file to
+	# determine whether a shell is valid (e.g.: `chsh` consults the
+	# `/etc/shells` to determine whether an unprivileged user may
+	# change the login shell for her own account).
+	#
+	# http://www.linuxfromscratch.org/blfs/view/7.4/postlfs/etcshells.html
+	
+	if ! grep "$newShellPath" < /etc/shells &> /dev/null; then
+		execute \
+			"printf '%s\n' '$newShellPath' | sudo tee -a /etc/shells" \
+			"Zsh (add '$newShellPath' in '/etc/shells')" \
+		|| return 1
+	fi
+	
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	# Set latest version of `Bash` as the default
+	# (macOS uses by default an older version of `Bash`).
+	
+	chsh -s "$newShellPath" &> /dev/null
+	print_result $? "Bash (use latest version)"
+	
+}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
-
-    print_in_purple "\n   Bash\n\n"
-
-    brew_install "Bash" "bash" \
-        && change_default_bash
-
-    brew_install "Bash Completion 2" "bash-completion@2"
+	
+	print_in_purple "\n   Shell\n\n"
+	
+	brew_install "Zsh" "zsh" \
+		&& change_default_shell
+	
+	#brew_install "Bash Completion 2" "bash-completion@2"
 
 }
 
