a52ed216afd237421b9c707381064578dc2877b4
Alexander Untch <alexander@untch.com>
2020-04-03 09:29:32 +0200

    Added zsh.sh

---
+++ src/os/install/macos/zsh.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n   Bash\n\n"
+
+    brew_install "Bash" "bash" \
+        && change_default_bash
+
+    brew_install "Bash Completion 2" "bash-completion@2"
+
+}
+
+main
