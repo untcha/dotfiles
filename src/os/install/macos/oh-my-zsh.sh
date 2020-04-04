bff9d95fffd087eeb7497e983c80e7c1c57ea1f6
Alexander Untch <alexander@untch.com>
2020-04-04 14:41:56 +0200

    Modified oh-my-zsh.sh

---
+++ src/os/install/macos/oh-my-zsh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+		&& . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+	
+	echo "oh-my-zsh blabla"
+	
+}
+
+main
