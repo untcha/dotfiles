f891e5db4328e04061707e05c393c0613b12faf3
Alexander Untch <alexander@untch.com>
2020-04-05 23:05:31 +0200

    Added lots of grep and sed magic to customize .zshrc

--- src/os/install/macos/ohmyz.sh
+++ src/os/install/macos/ohmyz.sh
@@ -18,19 +18,115 @@ install_oh-my-zsh() {
 
 customize_zshrc() {
 	
-	declare -r ZSHRC_PATH="$HOME/.zshrc"
+	declare -r ZSHRC_PATH="$HOME/.zshrc-test"
 	
 	local zshAutosuggestionsPath="/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 	local zshSyntaxHighlightingPath="/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 	
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-	execute \
-		"printf '%s\n' 'source $zshAutosuggestionsPath' | sudo tee -a '$ZSHRC_PATH'" \
-		"Oh My Zsh (add zsh-autosuggestions.zsh in '$ZSHRC_PATH')" \
+	# Uncomment '# ENABLE_CORRECTION="true"'
+	if grep -q "# ENABLE_CORRECTION" "$ZSHRC_PATH"; then
+		execute \
+			"sed -i '' '/ENABLE_CORRECTION/s/^# //g' '$ZSHRC_PATH'" \
+			"Uncommented: ENABLE_CORRECTION='true'" \
+		
+	fi
+	# Comment '# ENABLE_CORRECTION="true"'
+	# sed -i '' '/ENABLE_CORRECTION/s/^/# /g' "$ZSHRC_PATH";
 	
-	execute \
-		"printf '%s\n' 'source $zshSyntaxHighlightingPath' | sudo tee -a '$ZSHRC_PATH'" \
-		"Oh My Zsh (add zsh-syntax-highlighting.zsh in '$ZSHRC_PATH')" \
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	# Uncomment '# COMPLETION_WAITING_DOTS="true"'
+	if grep -q "# COMPLETION_WAITING_DOTS" "$ZSHRC_PATH"; then
+		execute \
+			"sed -i '' '/COMPLETION_WAITING_DOTS/s/^# //g' '$ZSHRC_PATH'" \
+			"Uncommented: COMPLETION_WAITING_DOTS='true'" \
+		
+	fi
+	
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	# Line number of the '# User configuration' line
+	local lineNumber=$(grep -n "# User configuration" $ZSHRC_PATH | grep -Eo '^[^:]+')
+	
+	# Add 'export PATH="$HOME/.jenv/bin:$PATH"'
+	if ! grep -q 'export PATH="$HOME/.jenv/bin:$PATH"' "$ZSHRC_PATH"; then
+		((lineNumber++))
+		sed -i '' $lineNumber'i\
+			export PATH="$HOME/.jenv/bin:$PATH"'$'\n' $ZSHRC_PATH
+		
+		print_success 'Added: export PATH="$HOME/.jenv/bin:$PATH"'
+	fi
+	
+	# Add 'eval "$(jenv init -)"'
+	if ! grep -q 'eval "$(jenv init -)"' "$ZSHRC_PATH"; then
+		((lineNumber++))
+		sed -i '' $lineNumber'i\
+			eval "$(jenv init -)"'$'\n' $ZSHRC_PATH
+		
+		print_success 'Added: eval "$(jenv init -)"'
+	fi
+	
+	# Add 'export PATH="/usr/local/sbin:$PATH"'
+	if ! grep -q 'export PATH="/usr/local/sbin:$PATH"' "$ZSHRC_PATH"; then
+		((lineNumber++))
+		sed -i '' $lineNumber'i\
+			export PATH="/usr/local/sbin:$PATH"'$'\n' $ZSHRC_PATH
+		
+		print_success 'Added: export PATH="/usr/local/sbin:$PATH"'
+	fi
+	
+	# Add 'export PATH="/usr/local/opt/curl/bin:$PATH"'
+	if ! grep -q 'export PATH="/usr/local/opt/curl/bin:$PATH"' "$ZSHRC_PATH"; then
+		((lineNumber++))
+		sed -i '' $lineNumber'i\
+			export PATH="/usr/local/opt/curl/bin:$PATH"'$'\n' $ZSHRC_PATH
+		
+		print_success 'Added: export PATH="/usr/local/opt/curl/bin:$PATH"'
+	fi
+	
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	# Uncomment '# export LANG=en_US.UTF-8'
+	if grep -q "# export LANG=en_US.UTF-8" "$ZSHRC_PATH"; then
+		execute \
+			"sed -i '' '/LANG=en_US.UTF-8/s/^# //g' '$ZSHRC_PATH'" \
+			"Uncommented: export LANG=en_US.UTF-8" \
+		
+	fi
+	
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	# Line number of the 'export LANG=en_US.UTF-8' line
+	local lineNumber=$(grep -n "export LANG=en_US.UTF-8" $ZSHRC_PATH | grep -Eo '^[^:]+')
+	
+	# Add 'export LC_ALL=en_US.UTF-8'
+	if ! grep -q 'export LC_ALL=en_US.UTF-8' "$ZSHRC_PATH"; then
+		((lineNumber++))
+		sed -i '' $lineNumber'i\
+			export LC_ALL=en_US.UTF-8'$'\n' $ZSHRC_PATH
+		
+		print_success 'Added: export LC_ALL=en_US.UTF-8'
+	fi
+	
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	# Add 'source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh' to .zshrc
+	if ! grep -q $zshAutosuggestionsPath "$ZSHRC_PATH"; then
+		execute \
+			"printf '\n%s' 'source $zshAutosuggestionsPath' | tee -a '$ZSHRC_PATH'" \
+			"Added: zsh-autosuggestions.zsh" \
+		
+	fi
+	
+	# Add 'source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' to .zshrc
+	if ! grep -q $zshSyntaxHighlightingPath "$ZSHRC_PATH"; then
+		execute \
+			"printf '\n%s' 'source $zshSyntaxHighlightingPath' | tee -a '$ZSHRC_PATH'" \
+			"Added: zsh-syntax-highlighting.zsh" \
+		
+	fi
 	
 }
 
