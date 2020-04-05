7aaf678132288d22241b65ba15ddb7dad5de91a9
Alexander Untch <alexander@untch.com>
2020-04-05 14:49:12 +0200

    Added customize_zshrc function

--- src/os/install/macos/ohmyz.sh
+++ src/os/install/macos/ohmyz.sh
@@ -16,6 +16,24 @@ install_oh-my-zsh() {
 	
 }
 
+customize_zshrc() {
+	
+	declare -r ZSHRC_PATH="$HOME/.zshrc"
+	
+	local zshAutosuggestionsPath="/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+	local zshSyntaxHighlightingPath="/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+	
+	
+	execute \
+		"printf '%s\n' 'source $zshAutosuggestionsPath' | sudo tee -a '$ZSHRC_PATH'" \
+		"Oh My Zsh (add zsh-autosuggestions.zsh in '$ZSHRC_PATH')" \
+	
+	execute \
+		"printf '%s\n' 'source $zshSyntaxHighlightingPath' | sudo tee -a '$ZSHRC_PATH'" \
+		"Oh My Zsh (add zsh-syntax-highlighting.zsh in '$ZSHRC_PATH')" \
+	
+}
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
@@ -26,6 +44,8 @@ main() {
 	
 	brew_install "zsh-autosuggestions" "zsh-autosuggestions"
 	brew_install "zsh-syntax-highlighting" "zsh-syntax-highlighting"
+	
+	customize_zshrc
 	
 }
 
