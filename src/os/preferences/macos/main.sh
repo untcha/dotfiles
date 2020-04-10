9b65315e0f136886f60d212cace82d5ccd196ac7
Alexander Untch <alexander@untch.com>
2020-04-10 19:11:47 +0200

    Added killall

--- src/os/preferences/macos/main.sh
+++ src/os/preferences/macos/main.sh
@@ -24,3 +24,16 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # System Preferences / Dock
 ./sp-dock.sh
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Kill affected applications
+
+for app in "Activity Monitor" \
+    "cfprefsd" \
+    "Dock" \
+    "Finder" \
+    "NotificationCenter" \
+    "SystemUIServer"; do
+    killall "${app}" &> /dev/null
+done
