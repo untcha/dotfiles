7a824e3b29f24e2c46ec833928e75d05ac37f714
Alexander Untch <alexander@untch.com>
2020-04-11 14:51:58 +0200

    Improved Path Finder

--- src/os/preferences/macos/cask-path-finder.sh
+++ src/os/preferences/macos/cask-path-finder.sh
@@ -12,7 +12,8 @@ execute "defaults write com.cocoatech.PathFinder globalAppsMenuEnabled -int 0 &&
 		 defaults write com.cocoatech.PathFinder PFShowsUndoPanel -int 0 && \
 		 defaults write com.cocoatech.PathFinder kArrowKeysLoopAround -int 1 && \
 		 defaults write com.cocoatech.PathFinder kCommandClickOpenBehavior -int 1 && \
-		 defaults delete com.cocoatech.PathFinder kNTNewWindowsViewType && \
 		 defaults write com.cocoatech.PathFinder kRevealInPathFinderBehaviorPrefKey -int 2 && \
 		 defaults write com.cocoatech.PathFinder useBase2MathForSizes -int 1" \
 	"Path Finder"
+
+# defaults delete com.cocoatech.PathFinder kNTNewWindowsViewType && \
