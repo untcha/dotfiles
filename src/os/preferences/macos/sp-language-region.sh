dd6a1431d95b032dee8ff85f65315528d6812d63
Alexander Untch <alexander@untch.com>
2020-04-11 12:27:45 +0200

    Improved sp-language-region.sh

--- src/os/preferences/macos/sp-language-region.sh
+++ src/os/preferences/macos/sp-language-region.sh
@@ -18,7 +18,6 @@ execute "defaults write NSGlobalDomain AppleLocale -string 'en_DE'" \
 # Locale for zsh will be set in .zshrc
 
 # Set 'Measurement units' to 'Centimeters' (Metric)
-execute "defaults write NSGlobalDomain AppleMeasurementUnits -string 'Centimeters'" \
-	"Set 'Measurement units' to 'Centimeters' (Metric)"
-execute "defaults write NSGlobalDomain AppleMetricUnits -int 1" \
+execute "defaults write NSGlobalDomain AppleMeasurementUnits -string 'Centimeters' \
+		 defaults write NSGlobalDomain AppleMetricUnits -int 1" \
 	"Set 'Measurement units' to 'Centimeters' (Metric)"
