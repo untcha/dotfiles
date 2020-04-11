5ea3061f933e7a84fee63e00e257999be9953cd6
Alexander Untch <alexander@untch.com>
2020-04-11 13:50:26 +0200

    Improved Amphetamine

--- src/os/preferences/macos/mas-amphetamine.sh
+++ src/os/preferences/macos/mas-amphetamine.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-execute "defaults write com.if.Amphetamine 'Status Item Click' = 2 && \
-		 defaults write com.if.Amphetamine 'Start Session At Launch' = 1 && \
-		 defaults write com.if.Amphetamine 'Start Session On Wake' = 1" \
+execute "defaults write com.if.Amphetamine 'Status Item Click' -int 2 && \
+		 defaults write com.if.Amphetamine 'Start Session At Launch' -int 1 && \
+		 defaults write com.if.Amphetamine 'Start Session On Wake' -int 1" \
 	"Amphetamine"
