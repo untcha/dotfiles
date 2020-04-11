7034af245ee4af1eebd34d6933dc3c0655ec8c97
Alexander Untch <alexander@untch.com>
2020-04-11 14:49:51 +0200

    Improved Path Finder

--- src/os/preferences/macos/cask-path-finder.sh
+++ src/os/preferences/macos/cask-path-finder.sh
@@ -8,5 +8,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 execute "defaults write com.cocoatech.PathFinder globalAppsMenuEnabled -int 0 && \
 		 defaults write com.cocoatech.PathFinder kOpenTextEditDocumentsInTextEditor -int 0 && \
 		 defaults write com.cocoatech.PathFinder kTerminalApplicationPath -string '/Applications/iTerm.app' && \
-		 defaults write com.cocoatech.PathFinder textEditorApplicationPath -string '/Applications/Textastic.app'" \
+		 defaults write com.cocoatech.PathFinder textEditorApplicationPath -string '/Applications/Textastic.app' && \
+		 defaults write com.cocoatech.PathFinder PFShowsUndoPanel -int 0 && \
+		 defaults write com.cocoatech.PathFinder kArrowKeysLoopAround -int 1 && \
+		 defaults write com.cocoatech.PathFinder kCommandClickOpenBehavior -int 1 && \
+		 defaults delete com.cocoatech.PathFinder kNTNewWindowsViewType && \
+		 defaults write com.cocoatech.PathFinder kRevealInPathFinderBehaviorPrefKey -int 2 && \
+		 defaults write com.cocoatech.PathFinder useBase2MathForSizes -int 1" \
 	"Path Finder"
