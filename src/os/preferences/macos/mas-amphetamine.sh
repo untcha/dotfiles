a296064c56fcc04799c6334ad7c64929a577414e
Alexander Untch <alexander@untch.com>
2020-04-11 14:10:15 +0200

    Improved Amphetamine

--- src/os/preferences/macos/mas-amphetamine.sh
+++ src/os/preferences/macos/mas-amphetamine.sh
@@ -7,5 +7,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 execute "defaults write com.if.Amphetamine 'Status Item Click' -int 2 && \
 		 defaults write com.if.Amphetamine 'Start Session At Launch' -int 1 && \
-		 defaults write com.if.Amphetamine 'Start Session On Wake' -int 1" \
+		 defaults write com.if.Amphetamine 'Start Session On Wake' -int 1 && \
+		 defaults write com.if.Amphetamine 'Always Ask to End For Low Battery' -int 1 && \
+		 defaults write com.if.Amphetamine 'End Session On Low Battery' -int 1 && \
+		 defaults write com.if.Amphetamine 'Ignore Battery on AC' -int 1 && \
+		 defaults write com.if.Amphetamine 'Restart DD Session on AC Reconnect' -int 1 && \
+		 defaults write com.if.Amphetamine 'Enable Session State Sound' -int 0" \
 	"Amphetamine"
+
+print_warning "Set 'Launch Amphetamine at Login' manually"
